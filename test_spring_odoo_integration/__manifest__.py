4e62c8e16fc145ea40d38d8579dc8e9d24e9d55f
odoospring <odoo@springsystems.com>
2026-06-12 20:00:24 -0400

    Add files via upload

---
+++ test_spring_odoo_integration/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Spring Systems EDI & Order Integration',
+    'version': '18.0.1.0.0',
+    'category': 'Sales',
+    'summary': 'Connect Odoo with major retailers (EDI), Shopify, and more for automated order, inventory, and fulfillment sync.',
+    'author': 'Spring Systems',
+    'website': 'https://springsystems.com/supplier-solutions/spring-integration',
+    'depends': ['sale', 'stock'],
+    'images': ['static/description/icon.png'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': True,
+}
