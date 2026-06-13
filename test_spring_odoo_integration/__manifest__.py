5898f41a2cdc87a65d5d41da340bb297ecb0e9ac
odoospring <odoo@springsystems.com>
2026-06-12 20:27:09 -0400

    Update __manifest__.py

--- test_spring_odoo_integration/__manifest__.py
+++ test_spring_odoo_integration/__manifest__.py
@@ -5,9 +5,14 @@
     'summary': 'Connect Odoo with major retailers (EDI), Shopify, and more for automated order, inventory, and fulfillment sync.',
     'author': 'Spring Systems',
     'website': 'https://springsystems.com/supplier-solutions/spring-integration',
+    'support': 'support@springsystems.com'
+    'live_test_url' : 'https://springsystems.com/email-us'
+    'price': 49.99,
+    'currency': 'USD'
     'depends': ['sale', 'stock'],
     'images': ['static/description/icon.png'],
     'license': 'LGPL-3',
     'installable': True,
     'application': True,
+   
 }
