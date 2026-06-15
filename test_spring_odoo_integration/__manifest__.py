bbf146ff41cf0a9d1521fbd045dc71251bd8f396
odoospring <odoo@springsystems.com>
2026-06-14 20:58:00 -0400

    Update __manifest__.py

--- test_spring_odoo_integration/__manifest__.py
+++ test_spring_odoo_integration/__manifest__.py
@@ -11,7 +11,7 @@
     'currency': 'USD',
     'depends': ['sale', 'stock'],
     'data': [
-        'static/description/index.xml',
+        'static/description/index.html',
     ],
     'images': ['static/description/icon.png'],
     'license': 'LGPL-3',
