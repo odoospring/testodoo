e8d851efdd206136979a9201be01de367c72653a
odoospring <odoo@springsystems.com>
2026-08-21 17:24:59 -0400

    Update __manifest__.py

--- test_spring_odoo_integration/__manifest__.py
+++ test_spring_odoo_integration/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Spring Systems EDI & Order Integration',
-    'version': '18.0.1.0.0',
+    'version': '19.0.1.0.0',
     'category': 'Sales',
     'summary': 'Connect Odoo with major retailers (EDI), Shopify, and more for automated order, inventory, and fulfillment sync.',
     'author': 'Spring Systems',
